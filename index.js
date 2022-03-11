13e9fe0887b2a0924cfb441d8ca3b8043e456dfa
Marcin Rataj <marcinrataj0@gmail.com>
2022-03-11 14:33:01 +0100

    enter value to check the price

---
+++ index.js
@@ -0,0 +1,9 @@
+//Symulacja modułów ( w przyszłości będziemy robić)
+//- każdy komponent w innym module (póki co pliku)
+//- index.js - do renderowania
+//- App.js - główny komponent, obsługuje stan
+//- Header.js - nagłówek
+//- ListItems - komponent będący kontenerem na listę potraw
+//- Item.js - Komponent na jedną potrawę
+
+// ReactDOM.render(<App/>, document.getElementById('root'))
