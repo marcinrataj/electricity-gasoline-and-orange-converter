92aef02f344d5419f838f2840456beee4f695c15
Marcin Rataj <marcinrataj0@gmail.com>
2022-03-16 20:10:49 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-//Symulacja modułów ( w przyszłości będziemy robić)
-//- każdy komponent w innym module (póki co pliku)
-//- index.js - do renderowania
-//- App.js - główny komponent, obsługuje stan
-//- Header.js - nagłówek
-//- ListItems - komponent będący kontenerem na listę potraw
-//- Item.js - Komponent na jedną potrawę
-
-// ReactDOM.render(<App/>, document.getElementById('root'))
